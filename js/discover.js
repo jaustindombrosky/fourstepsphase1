f2decacbe964a3964b9f823b751510466e98f759
Landon Johnson <landonwjohnson@gmail.com>
2018-07-20 17:06:04 -0600

    found more of the icons we are suppose to use, wrote function to make text red or green on cashflow networth labels and formated the numbers by currency

--- js/discover.js
+++ js/discover.js
@@ -3,6 +3,18 @@ let discoverTabWidth = window.getComputedStyle(discovertab).width;
 let discoverSideBar = document.getElementById('sidebar');
 let width = window.innerWidth;
 
+let networthValue = {
+    value: -14514,
+    id: 'networth-label'
+} 
+
+
+let cashflowValue = {
+    value: 1585,
+    id: 'cashflow-label'
+}
+
+let valuesArray = [networthValue, cashflowValue]
 
 var onresize = function() {
     width = document.body.clientWidth;
@@ -13,6 +25,28 @@ var onresize = function() {
     }
 }
 
+function handleCurrency(incomingCurrency){
+    let value = incomingCurrency.toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+    let spot = value.indexOf('.');
+    let newValue = value.slice(0, spot);
+    return newValue;
+}
+
+function handleValues(){
+    valuesArray.forEach(item => {
+        let idLabel = document.getElementById(item.id);
+        if(item.value > 0){
+
+            idLabel.style.color = "#B4E1CE"
+            idLabel.innerHTML = handleCurrency(item.value);
+
+        } else if(item.value < 0){
+
+            idLabel.style.color = "#D25956"
+            idLabel.innerHTML = handleCurrency(item.value);
+        }
+    })
+}
 
 function handleToggle(){
     if(discoverSideBar.className === "discover-sidebar"){
@@ -27,4 +61,14 @@ function toggleSideBar(){
     width < 629? handleToggle() : console.log('screen too big')
 }
 
+function toggleStrategy(id, activeClassName, disableClassName){
+    let strategy = document.getElementById(id);
+    if(strategy.className === activeClassName){
+        strategy.className = disableClassName;
+    } else if(strategy.className === disableClassName) {
+        strategy.className = activeClassName;
+    }
+}
+
 window.addEventListener("resize", onresize);
+window.onload = handleValues();
