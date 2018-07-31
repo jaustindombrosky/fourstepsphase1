9dfd811c7bee5a3ff3f1fbd5a3f7607774d7630e
jaustindombrosky <austindombrosky@gmail.com>
2018-07-31 11:42:24 -0600

    update

---
+++ app.js
@@ -0,0 +1,56 @@
+//THIS IS BOILERPLATE FOR US TO USE MONGOOSE AD 7/31/18
+
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mongoose = require('mongoose')
+
+//when deployed change the localhost
+var dbUrl = 'mongodb://localhost/profile-signup'
+mongoose.connect(dbUrl, function(err, res){
+  if (err){
+    console.log('DB connection failed')
+  }
+  else {
+    console.log('DB connection succeeded')
+
+  }
+})
+
+var routes = require('./routes/index');
+var api = require('./routes/api');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hjs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/api', api);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
